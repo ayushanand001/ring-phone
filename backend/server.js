c548f32a1d86e093f915233188523258603b614c
Ayush Anand <ayushanand10521@gmail.com>
2026-03-10 01:23:33 +0530

    minor update

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ app.use(
 const serviceAccount = require("./firebase-key.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 app.post("/ring", async (req, res) => {
